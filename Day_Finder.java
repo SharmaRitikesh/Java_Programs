f97404e4565267a4c6aa21c61b8eff716c6499e7
Ritikesh Sharma <78540540+SharmaRitikesh@users.noreply.github.com>
2024-02-17 00:06:44 +0530

    Day Finder through date

---
+++ Day_Finder.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.time.LocalDate;
+
+class function {
+    
+    public static String findDay(int month, int day, int year) {
+    LocalDate date = LocalDate.of(year, month, day);
+    return date.getDayOfWeek().toString();
+    }
+
+}
+
+public class Day_Finder {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = function.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
